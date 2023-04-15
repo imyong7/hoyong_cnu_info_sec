a14d646d66938fe73a2a9c74f28d507c02a7bd29
imyong7 <imyong7@hanmail.net>
2023-04-16 03:01:52 +0900

    modify client, server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,29 +1,62 @@
 use std::net::{TcpStream};
+use std::io::prelude::*;
 use std::io::{Read, Write};
 use std::str::from_utf8;
 
+use aes::Aes256;
+use aes::cipher::{
+    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
+    generic_array::GenericArray,
+};
+
+
 fn main() {
+    // let stdin = std::io::stdin();
+    // let stdin = stdin.lock();
+    // for line in stdin.lines() {
+    //     println!("{:?}", &line.unwrap());
+    // }
+
+    let key = GenericArray::from([0u8; 16]);
+    let mut block = GenericArray::from([42u8; 16]);
+
+    // Initialize cipher
+    let cipher = Aes256::new(&key);
+
+
+
     match TcpStream::connect("localhost:3333") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
 
-            let msg = b"Hello!";
+            // open stdin
+            let stdin = std::io::stdin();
+            let stdin = stdin.lock();
+            for line in stdin.lines() {
+                let msg = &line.unwrap();
 
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
+                stream.write(msg.as_bytes()).unwrap();
+                println!("Sent {}, awaiting reply...", msg);
 
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is Ok!");
-                    } else {
+                let mut data = vec![0 as u8; msg.len()]; // using dynamic length buffer
+
+                match stream.read_exact(&mut data) {
+                    Ok(_) => {
                         let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply : {}", text);
+
+                        if text == msg {
+                            println!("Reply is Ok!");
+                        } else {
+                            let text = from_utf8(&data).unwrap();
+                            println!("Unexpected reply : {}", text);
+                        }
+
+                        // let text = from_utf8(&data).unwrap();
+                        // println!("{}", text);
+                    },
+                    Err(e) => {
+                        println!("FAiled to receive data: {}", e);
                     }
-                },
-                Err(e) => {
-                    println!("FAiled to receive data: {}", e);
                 }
             }
         },
@@ -32,5 +65,5 @@ fn main() {
         }
     }
 
-    println!("Termincated.");
+    println!("Terminated.");
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,16 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::str::from_utf8;
+
 
 fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50];
+    let mut data = [0 as u8; 1024];
     while match stream.read(&mut data) {
         Ok(size) => {
+            let text = from_utf8(&data[0..size]).unwrap();
+            println!("From client data :: {}", text);
+
             stream.write(&data[0..size]).unwrap();
             true
         },
