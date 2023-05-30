dbe9ab8ae4c29a5bb1bcfc0ccae1dcb8ec3cc3e0
imyong7 <imyong7@hanmail.net>
2023-05-31 04:23:27 +0900

    - key distribution is completed - need to fix chat message

--- src/bin/client2.rs
+++ src/bin/client2.rs
@@ -14,6 +14,7 @@ use rand::Rng;
 use rsa::traits::PublicKeyParts;
 use std::hash::Hash;
 use rsa::pkcs8::{EncodePublicKey, DecodePublicKey};
+use try_catch::catch;
 
 
 // 서버에서 데이터를 받아서 복호화 하는 함수 - 스레드 생성해서 실행
@@ -86,7 +87,7 @@ fn handle_receive(mut stream: TcpStream, cipher: Aes256, nickname: String) {
 }
 */
 
-fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, nickname: String) {
+fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, mut recv_cipher: Aes256, nickname: String) {
     let mut data = vec![0 as u8; 1024]; // 수신할 데이터 블록 길이 설정
 
     while match stream.read(&mut data) {
@@ -102,6 +103,7 @@ fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, nickname: String) {
 
             // 복호화 할 블록 어레이 선언
             let mut blocks = vec![];
+            let mut _blocks = vec![];
 
             // 길이 :: 16으로 버퍼로 잘라서 복호화 수행
             for i in 0..msg_split_blocks {
@@ -118,11 +120,13 @@ fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, nickname: String) {
                 buffer[..split_unit].copy_from_slice(split_msg); // 버퍼에 문자 길이만큼 할당
 
                 let mut block = GenericArray::from( buffer ); // 버퍼를 복호화용 블록 어레이로 변환
+                let mut _block = GenericArray::from( buffer ); // 버퍼를 복호화용 블록 어레이로 변환
                 blocks.push(block);
+                _blocks.push(_block);
             }
 
-            // 블록 복호화
-            cipher.decrypt_blocks(&mut blocks);
+            // 블록 복호화, 수신한 비밀커로 복호화 오류 시 자가생성한 비밀키로 복호화
+            recv_cipher.decrypt_blocks(&mut blocks);
 
             // 블록의 형타입 변환
             let mut _block = [0 as u8; 1024]; // 블록 버퍼 zero-fill
@@ -140,9 +144,36 @@ fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, nickname: String) {
                 _dec_sliced_block = &_dec_sliced_block[0..index];
             }
 
-            // 바이트 배열을 합쳐서 문자열로 변환
-            let dec_text = from_utf8(_dec_sliced_block).unwrap(); // 복호화된 배열을 문자열로 변환
-            println!("{}", dec_text);
+            if from_utf8(_dec_sliced_block).is_ok() {
+                // 바이트 배열을 합쳐서 문자열로 변환
+                println!("UTF8 is ok");
+                let dec_text = from_utf8(_dec_sliced_block).unwrap(); // 복호화된 배열을 문자열로 변환
+                println!("{}", dec_text);
+            } else {
+                println!("UTF8 is no");
+                // 블록 복호화, 수신한 비밀커로 복호화 오류 시 자가생성한 비밀키로 복호화
+                cipher.decrypt_blocks(&mut _blocks);
+
+                // 블록의 형타입 변환
+                let mut _block = [0 as u8; 1024]; // 블록 버퍼 zero-fill
+                let mut index = 0;
+                for block in _blocks.iter() { // 배열에 블록 바이트 값 할당
+                    for byte in block.iter() {
+                        _block[index] = *byte;
+                        index = index + 1;
+                    }
+                }
+
+                // zero-fill 된 어레이 trimming
+                let mut _dec_sliced_block = _block.as_slice(); // 복호화된 배열을 형변환 수행
+                if let Some(index) = _dec_sliced_block.iter().position(|&i| i == 0) {
+                    _dec_sliced_block = &_dec_sliced_block[0..index];
+                }
+
+                // 바이트 배열을 합쳐서 문자열로 변환
+                let dec_text = from_utf8(_dec_sliced_block).unwrap(); // 복호화된 배열을 문자열로 변환
+                println!("{}", dec_text);
+            }
 
             true
         },
@@ -154,36 +185,6 @@ fn handle_receive(mut stream: TcpStream, mut cipher: Aes256, nickname: String) {
 }
 
 fn main() {
-    /*
-    // RSA 키 생성 시퀀스 시작
-    let mut rng = rand::thread_rng();
-    let bits = 2048; // RSA 비트 수 2048
-
-    // 개인키 생성
-    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-
-    // 공개키 생성
-    let public_key = RsaPublicKey::from(&private_key);
-    println!("public_key bytes :: {:?}, {:?}", public_key.to_public_key_der().unwrap().as_bytes(), public_key.to_public_key_der().unwrap().as_bytes().len());
-    println!("public_key data :: {:?}",  RsaPublicKey::from_public_key_der(public_key.to_public_key_der().unwrap().as_bytes()) );
-
-    // 상대 클라이언트측에서 복호화된 AES KEY
-    // let mut rsa_dec_aes_key;
-
-    // 대칭키(AES256) 생성
-    let mut keydata = GenericArray::from([0u8; 32]);
-    let mut key = GenericArray::from([0u8; 32]);
-    let mut cipher = Aes256::new(&key);
-    let mut _cipher = Aes256::new(&key);
-
-    // let mut recv_cipher;
-    // let mut _recv_cipher;
-
-    // 공개키로 AES 키 암호화
-    let enc_aes_key = public_key.encrypt(&mut rng, Pkcs1v15Encrypt, &key).expect("failed to encrypt");
-    println!("enc_aes_key :: {:?}, {:?}", enc_aes_key, enc_aes_key.len());
-    */
-
     // WORKING CODE !!!!!
     println!("Enter nickname...");
     let mut nickname = String::new(); // 닉네임 설정
@@ -197,15 +198,19 @@ fn main() {
 
     match TcpStream::connect("localhost:3333") {
         Ok(mut stream) => {
+            let mut _stream = stream.try_clone().unwrap();
             stream.write_all(nickname.as_bytes()).unwrap(); // 서버로 닉네임 들어왔다고 전송
             println!("Successfully connected to server in port 3333");
 
-            // loop {
             let mut client_data = vec![0 as u8; 1];
+            let mut arr_client = vec![];
+
             match stream.read(&mut client_data) {
                 Ok(_) => {
-                    if client_data[0] == 2 {
-                        println!("{:?} clients connected!\n", client_data);
+                    arr_client = vec![0 as u8; (client_data[0] as u8) as usize];
+
+                    if arr_client.len() == 2 {
+                        println!("{:?} clients connected!\n", arr_client.len());
                         // break;
                     }
                 }
@@ -213,7 +218,6 @@ fn main() {
                     // println!("error");
                 }
             }
-            // }
 
             println!("Key distribution in progress...");
             // RSA 키 생성 시퀀스 시작
@@ -229,28 +233,28 @@ fn main() {
             let mut public_key_bytes = public_key.to_public_key_der().unwrap();
 
             // 공개키 전송
-            stream.write_all(&public_key_bytes.as_bytes());
-            stream.flush();
+            _stream.write(&public_key_bytes.as_bytes());
+            _stream.flush();
+
+            // println!("public_key :: {:?}, {:?}\n", &public_key_bytes.as_bytes(), &public_key_bytes.as_bytes().len());
 
             // 수신한 상대방의 공개키 수신
-            let mut recv_rsa_pub_key_data = [0 as u8; 294];
-            // loop {
+            let mut recv_rsa_pub_key_data = vec![0 as u8; 294 * arr_client.len()];
             match stream.read(&mut recv_rsa_pub_key_data) {
                 Ok(_) => {
-                    if public_key_bytes.as_bytes() != recv_rsa_pub_key_data {
-                        println!("recv_public_key :: {:?}, {:?}\n", recv_rsa_pub_key_data, recv_rsa_pub_key_data.len());
-
-                        // 수신받은 공개키로 교체
-                        recv_public_key = RsaPublicKey::from_public_key_der(&*recv_rsa_pub_key_data.to_vec()).unwrap();
-                        println!("public_key :: {:?}, {:?}\n", &public_key_bytes.as_bytes(), &public_key_bytes.as_bytes().len());
-                        // break;
+                    for key_chunk in recv_rsa_pub_key_data.chunks(294) {
+                        // println!("recv_public_key - key chunk :: {:?}, {:?}\n", key_chunk, key_chunk.len());
+                        if public_key_bytes.as_bytes() != key_chunk {
+                            // 수신받은 공개키로 교체
+                            recv_public_key = RsaPublicKey::from_public_key_der(&*key_chunk.to_vec()).unwrap();
+                            // println!("recv_public_key :: {:?}, {:?}\n", key_chunk, key_chunk.len());
+                        }
                     }
-                }
+                },
                 Err(_) => {
                     println!("error");
                 }
             }
-            // }
             // thread::sleep(time::Duration::from_millis(3000));
 
             // 상대 클라이언트측에서 복호화된 AES KEY
@@ -270,45 +274,44 @@ fn main() {
 
             // 상대 공개키로 AES 키 암호화
             let rsa_enc_aes_key = recv_public_key.encrypt(&mut rng, Pkcs1v15Encrypt, &key).expect("failed to encrypt");
-            println!("rsa_enc_aes_key :: {:?}\n", rsa_enc_aes_key);
+            // println!("rsa_enc_aes_key :: {:?}\n", rsa_enc_aes_key);
 
             // 공개키로 암호화한 비밀키 전송
-            stream.write_all(&rsa_enc_aes_key);
-            stream.flush();
+            _stream.write(&rsa_enc_aes_key);
+            _stream.flush();
             // thread::sleep(time::Duration::from_millis(3000));
 
             // 교환된 상대방의 대칭키 수신
-            let mut recv_key_data = vec![0 as u8; 256];
-            // loop {
+            let mut recv_key_data = vec![0 as u8; 256 * arr_client.len()];
             match stream.read(&mut recv_key_data) {
                 Ok(_) => {
-                    if rsa_enc_aes_key != recv_key_data {
-                        println!("recv_key_data :: {:?}, {:?}\n", recv_key_data, recv_key_data.len());
+                    for key_chunk in recv_key_data.chunks(256) {
+                        // println!("recv_public_key - key chunk :: {:?}, {:?}\n", key_chunk, key_chunk.len());
+                        if rsa_enc_aes_key != key_chunk {
+                            // println!("key_chunk :: {:?}, {:?}\n", key_chunk, key_chunk.len());
 
-                        // 개인키로 복호화
-                        let dec_key = private_key.decrypt(Pkcs1v15Encrypt, &recv_key_data).expect("failed to decrypt");
+                            // 개인키로 복호화
+                            let dec_key = private_key.decrypt(Pkcs1v15Encrypt, &key_chunk).expect("failed to decrypt");
 
-                        // 키 데이터를 암호화에 맞는 형으로 변환
-                        let mut index = 0;
-                        for block in dec_key.iter() { // 배열에 블록 바이트 값 할당
-                            rsa_dec_aes_key[index] = *block;
-                            index = index + 1;
+                            // 키 데이터를 암호화에 맞는 형으로 변환
+                            let mut index = 0;
+                            for block in dec_key.iter() { // 배열에 블록 바이트 값 할당
+                                rsa_dec_aes_key[index] = *block;
+                                index = index + 1;
+                            }
+                            // println!("rsa_dec_aes_key :: {:?}, {:?}\n", rsa_dec_aes_key, rsa_dec_aes_key.len());
+
+                            // 서버에서 수신한 키로 수신용 복호화 AES 클래스 선언
+                            recv_cipher = Aes256::new(&rsa_dec_aes_key);
                         }
-
-                        println!("dec_key :: {:?}, {:?}\n", dec_key, dec_key.len());
-                        println!("rsa_dec_aes_key :: {:?}, {:?}\n", rsa_dec_aes_key, rsa_dec_aes_key.len());
-
-                        // 서버에서 수신한 키로 수신용 복호화 AES 클래스 선언
-                        recv_cipher = Aes256::new(&rsa_dec_aes_key);
-                        // break;
                     }
-                }
+                },
                 Err(_) => {
                     println!("error");
                 }
             }
-            // }
             println!("Key distribution completed...");
+            println!("------------------------------");
 
             //
 
@@ -316,7 +319,7 @@ fn main() {
 
             thread::spawn(move || {
                 // handle_receive(receive_stream, _cipher, _nickname);
-                handle_receive(receive_stream, recv_cipher, _nickname);
+                handle_receive(receive_stream, _cipher, recv_cipher, _nickname);
             });
 
             let stdin = std::io::stdin();
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -44,7 +44,7 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 // 비동기 설정(스트림 읽기에서 블로킹 방지)
-                // stream.set_nonblocking(true);
+                stream.set_nonblocking(false);
 
                 // 클라이언트에서 입력하는 닉네임 데이터 수신
                 let mut nickname_data = [0 as u8; 1024];
@@ -74,40 +74,52 @@ fn main() {
 
                     // 공개키 교환
                     println!("Public keys exchange");
-                    for _stream in clients.iter() {
-                        let mut __stream = _stream.try_clone().unwrap();
-                        let mut rsa_pub_data = vec![0 as u8; 294]; // 공개키 294 bytes
+                    let mut idx_rsa_pub_data = 0;
+                    let mut rsa_pub_data = vec![0 as u8; 294]; // 공개키 294 bytes
+                    let mut arr_rsa_pub_data = vec![0 as u8; 294 * clients.len()];
 
-                        match __stream.read(&mut rsa_pub_data) {
+                    for mut _stream in clients.iter() {
+                        match _stream.read(&mut rsa_pub_data) {
                             Ok(size) => {
-                                println!("rsa_pub_data :: {:?}", rsa_pub_data);
-
-                                __stream.write_all(&rsa_pub_data);
-                                // __stream.flush();
+                                // println!("rsa_pub_data :: {:?}", rsa_pub_data);
+                                for byte in rsa_pub_data.iter() {
+                                    arr_rsa_pub_data[idx_rsa_pub_data] = *byte;
+                                    idx_rsa_pub_data = idx_rsa_pub_data + 1;
+                                }
                             },
                             Err(e) => {}
-                        };
+                        }
                     }
 
+                    for mut _stream in clients.iter() {
+                        _stream.write(&arr_rsa_pub_data);
+                        // __stream.flush();
+                    }
 
 
                     // 각 클라이언트에서 RSA 공개키로 암호화 한 AES 키 교환
                     println!("Encrypted secret keys exchange");
-                    for _stream in clients.iter() {
-                        let mut __stream = _stream.try_clone().unwrap();
-                        let mut enc_aes_data = vec![0 as u8; 256]; // RSA 2048 -> 8로 나누면 바이트 길이는 256
+                    let mut idx_enc_aes_data = 0;
+                    let mut enc_aes_data = vec![0 as u8; 256]; // RSA 2048 -> 8로 나누면 바이트 길이는 256
+                    let mut arr_enc_aes_data = vec![0 as u8; 256 * clients.len()]; // RSA 2048 -> 8로 나누면 바이트 길이는 256
 
-                        match __stream.read(&mut enc_aes_data) {
+                    for mut _stream in clients.iter() {
+                        match _stream.read(&mut enc_aes_data) {
                             Ok(size) => {
-                                println!("rsa_pub_data :: {:?}", enc_aes_data);
-                                __stream.write_all(&enc_aes_data);
-                                // __stream.flush();
-                            },
+                                // println!("enc_aes_data :: {:?}", enc_aes_data);
+                                for byte in enc_aes_data.iter() {
+                                    arr_enc_aes_data[idx_enc_aes_data] = *byte;
+                                    idx_enc_aes_data = idx_enc_aes_data + 1;
+                                }
+                            }
                             Err(e) => {}
-                        };
+                        }
                     }
 
-
+                    for mut _stream in clients.iter() {
+                        _stream.write(&arr_enc_aes_data);
+                        // __stream.flush();
+                    }
 
                     // 클라이언트 TCP stream을 복제해서 스레드에서 실행할 수 있도록 전달
                     let mut cloned_clients = Vec::new();
